1bf7c8bb0a3b58a9ce7933dce11bc6f0169be21a
Harold Alberto Tascon <hardos34@hotmail.com>
2025-06-24 15:50:19 -0500

    Agregando actualizaciones de controladores de usuarios y helpers

--- controllers/user.js
+++ controllers/user.js
@@ -1,56 +1,72 @@
-const register = (req,res) =>{
-      
+const validate = require("../helpers/validate-user");
+
+const register = (req, res) => {
+
      let body = req.body;
 
+     try {
+
+          validate(body);
+
+     } catch (error) {
+
+          return res.status(400).json({
+          status: "error",
+          message: "Validacion de usuario no superada"
+          
+     });
+
+     }
+
      return res.status(200).json({
-       status:200,
-       message:"Accion Para registrar usuario",
-       data:body
-   }) 
+          status: 200,
+          message: "Accion Para registrar usuario",
+          data: body
+     })
 }
 
 
-const login = (req,res) =>{
+const login = (req, res) => {
      return res.status(200).json({
-       status:200,
-       message:"Accion Para Identificar usuario"
-   })
+          status: 200,
+          message: "Accion Para Identificar usuario"
+     })
 }
 
 
-const profile = (req,res) =>{
+const profile = (req, res) => {
      return res.status(200).json({
-       status:200,
-       message:"Accion Para registrar usuario"
-   })
+          status: 200,
+          message: "Accion Para registrar usuario"
+     })
 }
 
 
-const update = (req,res) =>{
+const update = (req, res) => {
      return res.status(200).json({
-       status:200,
-       message:"Accion Para registrar usuario"
-   })
+          status: 200,
+          message: "Accion Para registrar usuario"
+     })
 }
 
 
-const upload = (req,res) =>{
+const upload = (req, res) => {
      return res.status(200).json({
-       status:200,
-       message:"Accion Para registrar usuario"
-   })
+          status: 200,
+          message: "Accion Para registrar usuario"
+     })
 }
 
 
-const Avatar = (req,res) =>{
+const Avatar = (req, res) => {
      return res.status(200).json({
-       status:200,
-       message:"Accion Para registrar usuario"
-   })
+          status: 200,
+          message: "Accion Para registrar usuario"
+     })
 }
 
 
-module.exports={
+module.exports = {
      register,
      login,
      profile,
---
+++ helpers/validate-user.js
@@ -0,0 +1,32 @@
+const validator = require("validator");
+
+const validate = (params) => {
+    let validation = false;
+
+    let name = !validator.isEmpty(params.name) &&
+        validator.isLength(params.name, { min: 3, max: 50 }) &&
+        validator.isAlpha(params.name, "es-ES");
+
+    let email = !validator.isEmpty(params.email) &&
+        validator.isEmail(params.email);
+
+
+    let password = !validator.isEmpty(params.password) &&
+        validator.isLength(params.password, { min: 6, max: 150 }) 
+        ;
+
+
+    if (!name || !email || !password) {
+        throw new Error("No se ha superado la validacion");
+
+    } else {
+        console.log("validacion superada");
+        validation = true;
+    }
+
+    return validate;
+}
+
+
+
+module.exports = validate;
