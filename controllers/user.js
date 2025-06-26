6e5f92b9c768b8bb9aa0a428e14869efbb0d4bbf
Harold Alberto Tascon <hardos34@hotmail.com>
2025-06-25 19:04:03 -0500

    cambios de user

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const validate = require("../helpers/validate-user");
 const UserRepository = require('../repositories/user');
 const user = require("../models/user");
+const bcrypt = require('bcrypt');
 
 const register = async (req, res) => {
 
@@ -22,7 +23,7 @@ const register = async (req, res) => {
 
      try {
           console.log(body.email);
-          const existingUser = await UserRepository.findByEmail(body.email);
+          const existingUser = await UserRepository.findByEmail(body.email,1);
          
 
      } catch (e) {
@@ -35,6 +36,16 @@ const register = async (req, res) => {
           });
      }
 
+     let password = await bcrypt.hash(body.password, 10);
+     body.password= password;
+     body.email = body.email.toLowerCase();
+
+     try{
+          let userToSave= await UserRepository.create(body);
+     }catch(e){
+            console.log(e);
+     }
+
 
      return res.status(200).json({
           status: 200,
@@ -44,7 +55,38 @@ const register = async (req, res) => {
 }
 
 
-const login = (req, res) => {
+const login = async (req, res) => {
+
+     let body = req.body; 
+     if(!body.email || !body.password){
+          return res.status(400).json({
+          status: "error",
+          message: "Falta Datos por enviar"
+     });
+        
+     }
+     
+     
+      try {
+          console.log(body.email);
+          const User = await UserRepository.findByEmail(body.email,2);
+
+          let pwd = bcrypt.compareSync(body.password,User.password);
+
+          if(!pwd){
+               
+          }
+         
+
+     } catch (e) {
+              console.log(e);
+              return res.status(400).json({
+               status: "error",
+               message: "El usuario al buscar el usuario"
+              
+
+          });
+     } 
      return res.status(200).json({
           status: 200,
           message: "Accion Para Identificar usuario"
--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ class User {
     this.name = data.name || '';
     this.email = data.email || '';
     this.password = data.password || '';
+    this.perfil = data.perfil || 'PUB';
     this.created_at = data.created_at || new Date();
     this.updated_at = data.updated_at || null;
   }
