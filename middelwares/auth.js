aa4ad6520777280ba0f9a08a3ba10dcb3d97139e
Harold Alberto Tascon <hardos34@hotmail.com>
2025-06-25 20:18:00 -0500

    validaciones de Token

--- middelwares/auth.js
+++ middelwares/auth.js
@@ -1,3 +1,29 @@
-exports.auth = (req,res,next) =>{
-    
+const jwt = require("jwt-simple");
+const { secret } = require("../helpers/jwt");
+exports.auth = (req, res, next) => {
+    if (!req.headers.authorization) {
+
+        return res.status(404).json({
+            status:"error",
+            error :"La peticion no tiene autorizacion"
+        });
+
+    }
+
+    let token = req.headers.authorization.replace(/['"]+/g, "");
+    try {
+
+        let payLoad = jwt.decode(token,secret);
+
+        req.user = payLoad;
+
+
+
+    } catch (e) {
+        return res.status(404).json({
+            status: "error",
+            error: "Token Invalido"
+        });
+    }
+
 }
