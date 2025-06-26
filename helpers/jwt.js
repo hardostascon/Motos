b7d1220c06acf94d460f5f1e2eefa8c0bdb9a0e0
Harold Alberto Tascon <hardos34@hotmail.com>
2025-06-25 19:32:06 -0500

    agregando creacion de token

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,3 +2,14 @@ const jwt = require("jwt-simple");
 
 const secret = "Clave de mi backend de Node usando Postgres";
 
+const  createToken = (user) =>{
+    let now = Math.floor(Date.now()/1000);
+    let expiration = now +30*24*60*60;
+    let payLoad = {
+        id : user._id,
+        name: user.name, 
+        email:user.email, 
+        iat: now, 
+        exp: expiration
+    }
+}
