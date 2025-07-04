5ec7ddc1630746cfc0d74094c45f29c1b8321939
Harold Alberto Tascon <hardos34@hotmail.com>
2025-07-04 13:44:09 -0500

    inclusion nuevas rutas

--- routes/user.js
+++ routes/user.js
@@ -8,10 +8,8 @@ const {auth} =require("../middelwares/auth");
 router.post("/register",UserController.register);
 router.post("/login",UserController.login);
 
-router.get("/profile/:id",UserController.profile);
-router.put("/update",UserController.update);
-router.put("/upload/:id",UserController.upload);
+router.get("/profile/:id",auth,UserController.profile);
+router.put("/update",auth,UserController.update);
 
-router.get("/avatar/:file",UserController.Avatar);
 
 module.exports = router;
